45c8824bc16427d6d78695ef3e11771d6b1b0cb6
Nestoralons <nestoralons42@gmail.com>
2021-11-27 21:24:28 -0400

    Done

--- api/index.js
+++ api/index.js
@@ -19,10 +19,40 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const axios=require('axios')
+const {API_KEY} = process.env; 
+const {Genre} =require('./src/db.js')
+
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(3001, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
+/*
+* * Traemos la data de los generos de la api para guardarla en la base de datos
+*/
+ axios.get(`https://api.rawg.io/api/genres?key=${API_KEY}`)
+.then(response=>{
+ let a=response.data.results
+  let base= a.map(elemento=>
+
+    Genre.create({
+    Id:elemento.id,
+    Name:elemento.name
+    })
+  
+    );
+    Promise.all(base)
+    .then(()=>{
+      console.log('Data creada')
+    }
+   
+    )
+})
+.catch(error=>{
+  console.log(error)
+})
+
+    
   });
 });
